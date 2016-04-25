e0e12abec89ae525d48957bc3c71f5dc0a113319
Anna Clawson <seadeer@gmail.com>
2016-04-25 09:37:18 -0700

    little fixes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express();
 
 app.use(express.static(path.join(__dirname + '/client/')));
 app.use(express.static(path.join(__dirname + "/node_modules/")));
+app.use(bodyParser.json());
 
 require("./server/config/mongoose.js");
 require("./server/config/routes.js")(app);
